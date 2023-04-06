437b91ac60d613daed4953a75645ccdeac884141
artirajpoot377 <artirajpoot@377gmail.com>
2023-04-07 04:33:26 +0530

    dbconnected

---
+++ backend/db.js
@@ -0,0 +1,10 @@
+
+const mongoose = require('mongoose')
+const connectToMongo =  ()=>{
+    mongoose.connect( "mongodb+srv://aarurajpoot1999:8650148233@cluster0.x0csqqn.mongodb.net/project1",{
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+}
+module.exports = connectToMongo;
